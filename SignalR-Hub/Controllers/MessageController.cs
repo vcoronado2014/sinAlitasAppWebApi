00446e6879a5e4738b43e4657190c6ac4c957aad
vcoronado <vcoronado@rayensalud.com>
2018-11-29 12:48:41 -0300

    pruebas

---
+++ SignalR-Hub/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRHub;
+
+namespace SignalR_Hub.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Message")]
+    public class MessageController : Controller
+    {
+        private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
+        public MessageController(IHubContext<NotifyHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        [HttpPost]
+        public string Post([FromBody]Message msg)
+        {
+            string retMessage = string.Empty;
+
+            try
+            {
+                _hubContext.Clients.All.BroadcastMessage(msg.Type, msg.Payload);
+                retMessage = "Success";
+            }
+            catch (Exception e)
+            {
+                retMessage = e.ToString();
+            }
+
+            return retMessage;
+        }
+    }
+}
